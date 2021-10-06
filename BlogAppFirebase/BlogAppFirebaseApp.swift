3192d69eca608ce0d71c287cb508dc1e076b1d50
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-06 09:31:53 +0700

    Create the project

---
+++ BlogAppFirebase/BlogAppFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogAppFirebaseApp.swift
+//  BlogAppFirebase
+//
+//  Created by Danh Tu on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct BlogAppFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
