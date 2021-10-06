b3344828b02c972ed2bd68aaca93cbfdc776f397
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-06 10:12:13 +0700

    Building Home View

--- BlogAppFirebase/Views/Home.swift
+++ BlogAppFirebase/Views/Home.swift
@@ -8,12 +8,69 @@
 import SwiftUI
 
 struct Home: View {
+    @StateObject private var blogData = BlogViewModel()
+    
+    // Color based on ColorScheme
+    @Environment(\.colorScheme) var scheme
+    
     var body: some View {
-        List {
-            
+        VStack {
+            if let posts = blogData.posts {
+                // No Post
+                if posts.isEmpty {
+                    (
+                        Text(Image(systemName: "rectangle.and.pencil.and.ellipsis"))
+                        +
+                        Text("Start Writing Blog")
+                    )
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.primary)
+                } else {
+                    ProgressView()
+                }
+            } else {
+                ProgressView()
+                List {
+                    
+                }
+                .listStyle(.insetGrouped)
+            }
         }
-        .listStyle(.insetGrouped)
         .navigationTitle("My Blog")
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .overlay(
+            // FAB Button
+            Button(action: {
+                
+            }, label: {
+                Image(systemName: "plus")
+                    .font(.title2.bold())
+                    .foregroundColor(scheme == .dark ? Color.black : Color.white)
+                    .padding()
+                    .background(.primary, in: Circle())
+            })
+                .padding()
+                .foregroundStyle(.primary)
+            , alignment: .bottomTrailing
+        )
+    }
+    
+    @ViewBuilder
+    func CardView(post: Post) -> some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(post.title)
+                .fontWeight(.bold)
+            
+            Text("Written by: \(post.author)")
+                .font(.callout)
+                .foregroundColor(.gray)
+            
+            let postDate = post.date.dateValue().formatted(date: .numeric, time: .shortened)
+            Text("Written by: \(postDate)")
+                .font(.callout)
+                .foregroundColor(.gray)
+        }
     }
 }
 
