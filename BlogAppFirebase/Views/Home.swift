9a5cafe0607bb10c6edc7e11fe5d56b8f0a38a4b
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-06 09:42:26 +0700

    Architecture

---
+++ BlogAppFirebase/Views/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  BlogAppFirebase
+//
+//  Created by Danh Tu on 06/10/2021.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
